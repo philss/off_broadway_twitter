7f61da1d6ff12638eef48f390c391b52c8e36220
Philip Sampaio <philip.sampaio@gmail.com>
2021-01-06 02:15:28 -0300

    Removing logging messages

--- lib/off_broadway_twitter/producer.ex
+++ lib/off_broadway_twitter/producer.ex
@@ -16,7 +16,6 @@ defmodule OffBroadwayTwitter.Producer do
     uri = URI.parse(@twitter_stream_url_v2)
     token = Keyword.fetch!(opts, :twitter_bearer_token)
 
-    IO.puts("init => #{now()}")
     Process.send_after(self(), :start_stream, 2_000)
 
     {:producer,
@@ -35,7 +34,6 @@ defmodule OffBroadwayTwitter.Producer do
   @impl true
   def handle_info(:start_stream, state) do
     {:ok, conn} = HTTP2.connect(:https, state.uri.host, state.uri.port)
-    IO.puts("connected => #{now()}")
 
     {:ok, conn, request_ref} =
       HTTP2.request(
@@ -46,21 +44,7 @@ defmodule OffBroadwayTwitter.Producer do
         nil
       )
 
-    IO.puts("requested => #{now()}")
-
-    #send(self(), {:process_stream, conn})
-
-    IO.puts("started => #{now()}")
-
     {:noreply, [], %{state | request_ref: request_ref, conn: conn}}
-  end
-
-  @impl true
-  def handle_info({:process_stream, conn}, %{last_message: nil} = state) do
-    IO.puts("nil last message, #{now()}")
-    send(self(), {:process_stream, conn})
-
-    {:noreply, [], state}
   end
 
   @impl true
@@ -70,27 +54,20 @@ defmodule OffBroadwayTwitter.Producer do
         process_responses(resp, %{state | conn: conn})
 
       {:error, conn, %Mint.HTTPError{reason: {:server_closed_connection, :refused_stream, _}}, _} ->
-        IO.puts("disconnecting => #{now()}")
         Mint.HTTP.close(conn)
-        IO.puts("starting again => #{now()}")
         Process.send_after(self(), :start_stream, 100)
 
         {:noreply, [], %{state | conn: conn}}
 
       {:error, _conn, %{reason: reason}, _} when reason in [:closed, :einval] ->
-        IO.puts("ERROR unknown => #{reason}, #{now()}")
-        IO.puts("starting again => #{now()}")
         Process.send_after(self(), :start_stream, 2_000)
 
         {:noreply, [], %{state | conn: conn}}
 
       {:error, other} ->
-        IO.inspect(other, label: "stopping stream => #{now()}")
-
         {:stop, :stream_stopped, state}
 
       message ->
-        IO.inspect(message, label: "unknown stream result => #{now()}")
         {:noreply, [], state}
     end
   end
@@ -106,23 +83,14 @@ defmodule OffBroadwayTwitter.Producer do
 
   @impl true
   def handle_info({tag, _socket} = message, state) when tag in [:tcp_closed, :ssl_closed] do
-    IO.puts("closed SSL/TCP => #{now()}")
-
     {:noreply, [], %{state | last_message: message}}
   end
 
   @impl true
   def handle_info(message, state) do
-    IO.inspect(message, label: "unknown")
-
-    IO.puts("sending reconnecting => #{now()}")
     Process.send_after(self(), :start_stream, 1_000)
 
     {:noreply, [], state}
-  end
-
-  defp now do
-    DateTime.utc_now()
   end
 
   defp process_responses(responses, state) do
@@ -151,12 +119,9 @@ defmodule OffBroadwayTwitter.Producer do
             end
 
           {:done, _ref} ->
-            IO.puts("reconnecting after done, #{now()}")
-            # Process.send_after(self(), :start_stream, 1_000)
             state
 
-          {_, _ref, other} ->
-            IO.inspect(other, label: "other things")
+          {_, _ref, _other} ->
             state
         end
       end)
@@ -166,9 +131,6 @@ defmodule OffBroadwayTwitter.Producer do
 
   @impl true
   def handle_demand(demand, state) do
-    #IO.inspect(state.size, label: "size")
-    #IO.inspect(state.demand + demand, label: "demand")
-
     handle_received_messages(%{state | demand: state.demand + demand})
   end
 
