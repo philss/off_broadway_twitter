e942319cc97635bdbd42a650756ccdb6f3d45dbd
Philip Sampaio Silva <philip.sampaio@gmail.com>
2021-01-06 15:54:41 -0300

    Better handle timer in case of stopping the producer

--- lib/off_broadway_twitter/producer.ex
+++ lib/off_broadway_twitter/producer.ex
@@ -18,7 +18,7 @@ defmodule OffBroadwayTwitter.Producer do
     uri = URI.parse(@twitter_stream_url_v2)
     token = Keyword.fetch!(opts, :twitter_bearer_token)
 
-    Process.send_after(self(), :start_stream, @connect_in_ms)
+    timer = schedule_connection(@connect_in_ms)
 
     {:producer,
      %{
@@ -29,12 +29,23 @@ defmodule OffBroadwayTwitter.Producer do
        last_message: nil,
        conn: nil,
        token: token,
+       connection_timer: timer,
        uri: uri
      }}
   end
 
+  defp schedule_connection(interval) do
+    Process.send_after(self(), :connect_to_stream, interval)
+  end
+
+  @impl Producer
+  def prepare_for_draining(%{connection_timer: connection_timer} = state) do
+    connection_timer && Process.cancel_timer(connection_timer)
+    {:noreply, [], %{state | connection_timer: nil}}
+  end
+
   @impl true
-  def handle_info(:start_stream, state) do
+  def handle_info(:connect_to_stream, state) do
     {:ok, conn} = HTTP2.connect(:https, state.uri.host, state.uri.port)
 
     {:ok, conn, request_ref} =
@@ -46,7 +57,7 @@ defmodule OffBroadwayTwitter.Producer do
         nil
       )
 
-    {:noreply, [], %{state | request_ref: request_ref, conn: conn}}
+    {:noreply, [], %{state | request_ref: request_ref, conn: conn, connection_timer: nil}}
   end
 
   @impl true
@@ -57,20 +68,20 @@ defmodule OffBroadwayTwitter.Producer do
 
       {:error, conn, %Mint.HTTPError{reason: {:server_closed_connection, :refused_stream, _}}, _} ->
         # Do we need to close on our side?
-        Mint.HTTP.close(conn)
+        HTTP2.close(conn)
 
-        Process.send_after(self(), :start_stream, @reconnect_in_ms)
+        timer = schedule_connection(@reconnect_in_ms)
 
-        {:noreply, [], %{state | conn: conn}}
+        {:noreply, [], %{state | conn: conn, connection_timer: timer}}
 
       {:error, conn, %{reason: reason}, _} when reason in [:closed, :einval] ->
         # I got :einval once, but I suppose I was doing something wrong
-        Process.send_after(self(), :start_stream, @connect_in_ms)
+        timer = schedule_connection(@connect_in_ms)
 
-        {:noreply, [], %{state | conn: conn}}
+        {:noreply, [], %{state | conn: conn, connection_timer: timer}}
 
-      {:error, other} ->
-        {:stop, :stream_stopped, state}
+      {:error, _other} ->
+        {:stop, :stream_stopped_due_unknown_error, state}
     end
   end
 
